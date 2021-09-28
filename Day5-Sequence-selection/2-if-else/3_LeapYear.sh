af6836082ac228377a410f9a48fa706930ceea60
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-09-28 11:44:44 +0530

    Check if  Leap year or not

---
+++ Day5-Sequence-selection/2-if-else/3_LeapYear.sh
@@ -0,0 +1,14 @@
+#!   /bin/bash  -x
+
+
+
+read -p "enter the year in  YYYY format :" year
+
+
+
+if(( year % 4 == 0 && year % 100 !=0 || year % 400 == 0))
+then
+    echo "$year is leap year"
+else
+    echo "$year is not a leap year"
+fi
